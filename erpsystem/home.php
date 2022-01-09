0dcd07abfe2573a92b66f0b3389255257a68545b
BintangAdhi14 <adiwicaksono41@gmail.com>
2022-01-10 00:50:49 +0700

    second commit

---
+++ erpsystem/home.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<title>HOME AKADEMIK</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+  <style>
+  body {
+      background: url('assets/images/Background.jpg') left top no-repeat, url('assets/images/Background.jpg') right bottom no-repeat, url('assets/images/Background.jpg') left top repeat;
+      padding: 80px;
+      background-size: 10px, 10px, auto;
+    }
+  </style>
+  <div class="w3-container w3-white">
+  <div class="w3-bar">
+  <a href="#" class="w3-bar-item w3-button w3-mobile w3-left">TENTANG IPB</a>
+  <a href="#" class="w3-bar-item w3-button w3-mobile w3-center">PROGRAM</a>
+  <a href="#" class="w3-bar-item w3-button w3-mobile w3-center">VISI MISI</a>
+  <br><br>
+<div class="w3-container w3-blue">
+  <h1>IPB University</h1>
+  <p>Inspiring Innovation with Integrity</p>
+</div>
+<body>
+<div class="w3-container w3-green">
+<div class="w3-bar">
+
+</div>
+
+</div>
+</body>
+<center><style>
+h1 {
+  text-shadow: 2px 1px Green;
+}
+</style>
+  <img src="assets/images/download.png" width="250px height="200px"" /> <br>
+  <h1><font Size="12" face="Verdana">Akademik bisnis</font> <br>
+  <font Size="8" face="Verdana">Institute Pertanian Bogor </font></h1><br>
+</center>
+
+<body>
+  <style>
+video {
+  width: 100%;
+  height: auto;
+}
+  </style>
+  <body>
+
+  <video width="200px" height="200px" controls autoplay>
+    <source src="assets/media/IPB.mp4" type="video/mp4">
+    <source src="assets/media/IPB.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+  </video>
+  </body>
+<center><style>
+h2 {
+  text-shadow: 2px 1px blue;
+}
+</style>
+<div class="w3-row-padding w3-center w3-animate-top">
+    <h2>ABOUT IPB UNIVERSITY</h2>
+
+    <p>IPB UNIVERSITY is an agricultural higher education institution which historically was the
+      formation of secondary and higher education institutions in agriculture and veterinary medicine
+      which began in the early 20th century in Bogor.</p>
+      IPB UNIVERSITY offers twelve faculties programs Agriculture,
+      Veterinary Medicine, Fisheries and Marine Science, Animal Science,<br>Forestry
+      , Agricultural Technology, Mathematics and Natural Science, Economics and Management, Human Ecology, etc.
+
+
+</center>
+
+<div class="w3-row-padding w3-animate-left">
+    <h2>Vision</h2>
+    <p>To become a research-based higher education of international standard and prime mover of agriculture mainstreaming</p><br><br>
+
+    <h2>Mission</h2>
+    <ul style="list-style-type:circle;">
+        <li>To implement an excellent high education and comprehensive student development to produce competitive graduates with Indonesian characteristics.</li>
+        <li>To conduct research for the development of science and technology which bring benefit to agrarian and maritime communities..</li>
+        <li>To provide public services that promotes innovations in science and technology with entrepreneurial characteristics while maintaining the great values of the nation and preserving natural resources.</li>
+        <li>To strengthen an effective, efficient, transparent, and accountable management system of higher education.</li>
+    </ul>
+    <br><br>
+
+    <body>
+  <div class="w3-container">
+    <button onclick="myFunction('Demo1')" class="w3-btn w3-block w3-blue w3-left-align">Program Studi</button>
+    <div id="Demo1" class="w3-container w3-hide">
+      <h4>Undergraduate Program (S1)</h4>
+      <p>Bogor Agricultural University was established to be a center of excellence in agriculture (in a broad perspective) and promote scientific research benefiting the future of mankind. Bogor Agricultural University,</p>
+
+      <h4>Postgraduate Program (S2)</h4>
+      <p>the first School of Post Graduate Studies was establishedin Indonesia and IPB University was the pioneer in establishing such kind of graduate school.</p>
+    </div>
+
+    </div>
+    <script>
+    function myFunction(id) {
+      var x = document.getElementById(id);
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else {
+        x.className = x.className.replace(" w3-show", "");
+      }
+    }
+    </script>
+    </body>
